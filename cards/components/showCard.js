d5e21140ed2d51ad32ff10e035ac818dff3bce3c
Irina22-04 <baranovskaya.irina.87@mail.ru>
2020-05-22 17:39:48 +0300

    solved homework 4 add task

--- cards/components/showCard.js
+++ cards/components/showCard.js
@@ -1,39 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import './showCard.css';
 
-class ShowCard extends React.Component {
-
-    static propTypes = {
-        horisontal: PropTypes.number,
-        vertical: PropTypes.number,
-    };
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            horisontal: this.props.horisontal,
-            vertical: this.props.vertical,
-    }
-    }
-
-    componentDidUpdate(prevProps) {
-        if (this.props.horisontal !== prevProps.horisontal || this.props.vertical !== prevProps.vertical) {
-            this.setState({
-                horisontal: this.props.horisontal,
-                vertical: this.props.vertical,
-            });
-        }
-    }
-
-    render() {
-        const horisontalValue = 33.5 * (this.state.horisontal - 1);
-        const verticalValue = 7.7 * (this.state.vertical - 1);
-        return (
-            <div className={'card'} style={{backgroundPosition: `${horisontalValue}% ${verticalValue}%`}}/>
-        );
-    }
+function ShowCard(props) {
+    return (<div
+        className={'card'}
+        style={{backgroundPosition: `${33.5 * (props.horisontal - 1)}% ${7.7 * (props.vertical - 1)}%`}}
+    />)
 }
 
 export default ShowCard;
